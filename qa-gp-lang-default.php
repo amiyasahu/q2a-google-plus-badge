476f3929413ed3fb1c1bc1308c8809f6304e57df
Amiya Sahu <amiya053@gmail.com>
2014-07-12 15:51:14 +0530

    Language file updates

--- qa-gp-lang-default.php
+++ qa-gp-lang-default.php
@@ -32,26 +32,18 @@ if (!defined('QA_VERSION')) {
 }
 
 return array(
-	// languages for the facebook like modal 
-	"ur_google_page_url"        => "Page Url : The absolute URL of the Google Plus Page/Profile/Community that will be displayed . This is a required setting." ,
-	"settings_saved"        => "Google Plus Badge settings has been saved " ,
-	"plz_provide_gp_url"    => "Please provide your Google+ Page/Profile/Community url to display on website " ,
-	// languages for the facebook like box 
-	"show_gp_badge"      => "Show Google+ Badge " ,
+	"ur_google_page_url"          => "Page Url : The absolute URL of the Google Plus Page/Profile/Community that will be displayed . This is a required setting." ,
+	"settings_saved"              => "Google+ Badge settings has been saved " ,
+	"plz_provide_gp_url"          => "Please provide your Google+ Page/Profile/Community url to display on website " ,
+	"show_gp_badge"               => "Show Google+ Badge " ,
 	"ami_gp_badge_url_lable"      => "URL to the Google+ page" ,
-	"layout_label"               => "Orientation of the badge" ,
-	"portrait"                   => "Portrait" ,
-	"landscape"                  => "Landscape" ,
-	"ami_gp_showcoverphoto_label" => "Displays the cover photo in the badge if set to true and the photo exists." ,
-	"showtagline_label"          => "Displays the user's tag line if set to true." ,
-	"theme_label"                => "The color theme of the badge. Use dark when placing the badge on a page with a dark background." ,
-	"ami_gp_badge_width_label"    => "The pixel width of the badge to render.(Recomended 360px )" ,
+	"layout_label"                => "Orientation of the badge" ,
+	"ami_gp_showcoverphoto_label" => "Display the cover photo in the badge (the photo should exists) " ,
+	"showtagline_label"           => "Display the user's tag line " ,
+	"theme_label"                 => "The color scheme of the badge widget " ,
+	"ami_gp_badge_width_label"    => "Width of the badge to render in pixels" ,
 	"ami_gp_badge_type_lable"     => "Versions of the badges" ,
-	"ami_gp_show_owners_label"    => "Displays a list of community owners if set to true. (only applicable for communities )" ,
-	"ami_gp_showphoto_label"      => "Displays a list of community owners if set to true. (only applicable for communities )" ,
-	"gp_badge"                   => "Google Plus Badge" ,
-	"ami_show_gp_badge_header"                   => "Show widget header" ,
-	"ami_gp_badge_costum_header"                   => "Costum Header" ,
-	"ami_gp_badge_default_header"                   => "Follow us on Goole+" ,
-
+	"ami_gp_show_owners_label"    => "Display a list of community owners (only applicable for communities )" ,
+	"ami_gp_showphoto_label"      => "Display the community profile photo if the photo exists (only applicable for communities )" ,
+	"gp_badge"                    => "Google+ Badge" ,
 );
